f9311de590d179c7acf93cddc2c7a03bb1f7eecf
User <he1ex-tg@yandex.ru>
2023-08-23 18:55:11 +0500

    Adjust token generation

--- src/main/kotlin/tacos/authorizationserver/security/AuthorizationServerConfig.kt
+++ src/main/kotlin/tacos/authorizationserver/security/AuthorizationServerConfig.kt
@@ -3,30 +3,29 @@ package tacos.authorizationserver.security
 import com.nimbusds.jose.jwk.JWKSet
 import com.nimbusds.jose.jwk.RSAKey
 import com.nimbusds.jose.jwk.source.JWKSource
+import com.nimbusds.jose.proc.SecurityContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.core.context.SecurityContext
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.oauth2.core.AuthorizationGrantType
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod
 import org.springframework.security.oauth2.core.oidc.OidcScopes
+import org.springframework.security.oauth2.jwt.JwtDecoder
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings
 import org.springframework.security.web.SecurityFilterChain
-import java.math.BigInteger
 import java.security.KeyPair
 import java.security.KeyPairGenerator
 import java.security.interfaces.RSAPrivateKey
 import java.security.interfaces.RSAPublicKey
-import java.util.UUID
+import java.util.*
 
 @Configuration(proxyBeanMethods = false)
 class AuthorizationServerConfig {
@@ -34,7 +33,7 @@ class AuthorizationServerConfig {
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
     fun authorizationServerSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        OAuth2AuthorizationServerConfiguration()
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http)
         return http
             .formLogin(Customizer.withDefaults())
             .build()
@@ -57,14 +56,14 @@ class AuthorizationServerConfig {
         return InMemoryRegisteredClientRepository(registeredClient)
     }
 
-    /*@Bean
+    @Bean
     fun jwkSource(): JWKSource<SecurityContext> {
         val rsaKey = generateRsa()
         val jwkSet = JWKSet(rsaKey)
-        return {
-
+        return JWKSource<SecurityContext> { jwkSelector, _ ->
+            jwkSelector.select(jwkSet)
         }
-    }*/
+    }
 
     private fun generateRsa(): RSAKey {
         val keyPair = generateRsaKey()
@@ -80,5 +79,10 @@ class AuthorizationServerConfig {
         val keyPairGenerator = KeyPairGenerator.getInstance("RSA")
         keyPairGenerator.initialize(2048)
         return keyPairGenerator.generateKeyPair()
+    }
+
+    @Bean
+    fun jwtDecoder(jwkSource: JWKSource<SecurityContext>): JwtDecoder {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource)
     }
 }
