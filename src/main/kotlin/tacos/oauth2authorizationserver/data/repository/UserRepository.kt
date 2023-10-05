e98e754a2829b6ed7f0fd37efea1abe46f7ea3cc
User <he1ex-tg@yandex.ru>
2023-10-05 17:05:07 +0500

    Add getUserByUsername method

--- src/main/kotlin/tacos/oauth2authorizationserver/data/repository/UserRepository.kt
+++ src/main/kotlin/tacos/oauth2authorizationserver/data/repository/UserRepository.kt
@@ -2,5 +2,9 @@ package tacos.oauth2authorizationserver.data.repository
 
 import org.springframework.data.repository.CrudRepository
 import tacos.oauth2authorizationserver.data.entity.User
+import java.util.Optional
 
-interface UserRepository : CrudRepository<User, String>
+interface UserRepository : CrudRepository<User, String> {
+
+    fun getUserByUsername(username: String): Optional<User>
+}
